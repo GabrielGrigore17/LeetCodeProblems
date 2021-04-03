b7c82b540cf12edeb1d4c924c7d78df2c1f3084a
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-03 10:43:45 +0300

    Finished the first problem

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TwoSum
 {
     class Program
     {
+        static public int[] TwoSum(int[] nums, int target) 
+        {
+           var map = new Dictionary<int, int>();
+           for (int i = 0; i < nums.Length; i++){
+               int complement = target - nums[i];
+               if(map.ContainsKey(complement)){
+                   return new int[] {map[complement], i};
+               }
+               map.Add(nums[i], i);
+           }
+           throw new ArgumentException("The arguments parsed are not valid!");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nums1 = new int[]{2, 7, 11, 15};
+            var nums2 = new int[]{3, 2, 4};
+            var nums3 = new int[]{3, 3};
+
+            var res1 = TwoSum(nums1, 9);
+
+            foreach(var num in res1)
+                Console.WriteLine(num);
         }
     }
 }
