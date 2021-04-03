0596dc1d6e0d4f8c3d9ada0d57f86243ecc377d1
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-03 11:04:10 +0300

    Finished the second problem

--- TwoSumTwo/Program.cs
+++ TwoSumTwo/Program.cs
@@ -4,9 +4,28 @@ namespace TwoSumTwo
 {
     class Program
     {
+        static public int[] TwoSum(int[] numbers, int target){
+            var endSearchIndex = numbers.Length - 1;
+            for (int i = 0; i < numbers.Length; i++){
+                var complement = target - numbers[i];
+                for(int j = endSearchIndex; j >= 0; j--){
+                    if(numbers[j] > complement)
+                        endSearchIndex = j-1;
+                    else
+                        if(numbers[j] == complement)
+                            return new int[] {i+1, j+1};
+                        else
+                            break;
+                }
+            }
+            throw new ArgumentException("The arguments are not valid");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nums1 = new int[]{2, 7, 11, 15};
+            var res1 = TwoSum(nums1, 9);
+            foreach(var num in res1)
+                Console.WriteLine(num);
         }
     }
 }
