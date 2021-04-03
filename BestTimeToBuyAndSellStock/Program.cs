6396a234aca53952c658ea9bbeeb97b36ff5d5a7
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-03 11:32:31 +0300

    Finished the third problem

---
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BestTimeToBuyAndSellStock
+{
+    class Program
+    {
+        static public int MaxProfit(int[] prices) 
+        {
+            var minPrice = int.MaxValue;
+            var maxProfit = 0;
+            for(int i = 0; i < prices.Length; i ++)
+            {
+                if(prices[i] < minPrice)
+                    minPrice = prices[i];
+                else if (prices[i] - minPrice > maxProfit)
+                    maxProfit = prices[i] - minPrice;
+            }
+            return maxProfit;
+        }
+        static void Main(string[] args)
+        {
+            var prices = new int[]{7,1,5,3,6,4};
+            Console.WriteLine(MaxProfit(prices));
+        }
+    }
+}
