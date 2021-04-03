ea4adc69697ece9d1dd668faed2256b32641d676
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-03 16:25:37 +0300

    Completed

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        public static bool IsValid(string s) {
+            var numbers = new Stack<char>();
+            for(int i = 0; i < s.Length; i++){
+                switch(s[i]){
+                    case '{':
+                        numbers.Push('{');
+                        break;
+                    case '(':
+                        numbers.Push('(');
+                        break;
+                    case '[':
+                        numbers.Push('[');
+                        break;
+                    case ']':
+                        if(numbers.Count != 0){
+                            if(numbers.Peek() == '[')
+                                numbers.Pop();
+                            else
+                                return false;
+                        }
+                        else return false;
+                        break;
+                    case ')':
+                        if(numbers.Count != 0){
+                            if(numbers.Peek() == '(')
+                                numbers.Pop();
+                            else
+                                return false;
+                        }
+                        else return false;
+                        break;
+                    case '}':
+                        if(numbers.Count != 0){
+                            if(numbers.Peek() == '{')
+                                numbers.Pop();
+                            else
+                                return false;
+                        }
+                        else return false;
+                        break;
+                    default:
+                        throw new ArgumentException("The list of arguments is not valid");
+                }
+            }
+            if(numbers.Count == 0)
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("}(){}}{"));
+        }
+    }
+}
---
+++ VerifyingAnAlienDictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VerifyingAnAlienDictionary
+{
+    class Program
+    {
+        public static bool IsAlienSorted(string[] words, string order) {
+        
+        for(int i = 0; i<words.Length-1; i++){
+            
+            var first = words[i];
+            var second = words[i+1];
+            
+            if(!Compare(first, second, order)){
+                return false;
+            }
+        }
+        return true;
+        }
+        public static bool Compare(string first, string second, string order){
+        
+            for(int i = 0; i< Math.Min(first.Length, second.Length); i++){
+                
+                if(order.IndexOf(first[i]) == order.IndexOf(second[i]) ){
+                    continue;
+                }else if(order.IndexOf(first[i]) < order.IndexOf(second[i]) ){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+            return first.Length < second.Length;
+        }
+    
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAlienSorted(new string[]{"word","world","row"}, "worldabcefghijkmnpqstuvxyz"));
+        }
+    }
+}
