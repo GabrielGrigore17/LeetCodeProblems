a7c00a6690cb471f34ad502ee17b70953d285d52
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-05 08:43:43 +0300

    Completed a problem

---
+++ MergeSortedArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MergeSortedArray
+{
+    class Program
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n) 
+        {
+            for(int i = m; i < n + m; i++)
+            {
+                nums1[i] = nums2[i-m];
+            }
+            Array.Sort(nums1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
