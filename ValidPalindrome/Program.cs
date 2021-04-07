c3771a8693fa0275b89c80cee1895ff0a87091ee
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-07 10:03:54 +0300

    Completed a problem

---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ValidPalindrome
+{
+    class Program
+    {
+        public static bool IsPalindrome(string s) {
+            var str = FormatString(s);
+            for(int i = 0; i < str.Length/2; i++){
+                if(str[i] != str[str.Length - i - 1])
+                    return false;
+            }
+            return true;
+
+        }
+        public static string FormatString(string str){
+            char[] arr = str.Where(c => (char.IsLetterOrDigit(c))).ToArray(); 
+            str = (new string(arr)).ToLower();
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FormatString("A man, a plan, a canal: Panama"));
+            Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+        }
+    }
+}
