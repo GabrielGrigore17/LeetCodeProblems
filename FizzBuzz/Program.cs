ae6aa51d024a8b34fe7e4e3423508abbfcc25b78
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-08 19:55:52 +0300

    Solved FizzBuzz I am now a senior

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzz
 {
     class Program
     {
+        public static IList<string> FizzBuzz(int n) {
+            var output = new string[n];
+            for(int i = 1; i <= n; i++)
+            {
+                if(i % 15 == 0){
+                    output[i-1] = "FizzBuzz";
+                    continue;
+                }
+                if(i % 3 == 0)
+                {
+                    output[i-1] = "Fizz";
+                    continue;
+                }
+                if(i % 5 == 0)
+                {
+                    output[i-1] = "Buzz";
+                    continue;
+                }
+                output[i-1] = i.ToString();
+            }
+            return output;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
