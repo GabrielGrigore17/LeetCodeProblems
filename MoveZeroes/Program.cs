9a9c966fb9f69a72c84dc5c889969b55c1a9764a
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-08 20:35:14 +0300

    Completed a problem

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MoveZeroes
+{
+    class Program
+    {
+        public static void MoveZeroes(int[] nums) {
+            
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int numberOfZeroes = 0;
+                while(nums[i] == 0 && i + numberOfZeroes + 1 < nums.Length){
+                    numberOfZeroes++;
+                    var aux = nums[i];
+                    nums[i] = nums[i + numberOfZeroes];
+                    nums[i + numberOfZeroes] = aux;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var nums = new int[]{0,1,0,3,12};
+            MoveZeroes(nums);
+            foreach(var num in nums){
+                Console.Write(num);
+                Console.Write(" ");
+            }
+        }
+    }
+}
