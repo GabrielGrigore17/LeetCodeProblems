47951aabbadf004f7a192413b551432f72e072ca
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-09 19:30:27 +0300

    Completed a problem

---
+++ ClimbingStairs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClimbingStairs
+{
+    class Program
+    {
+        public int ClimbStairs(int n) {
+            int sum = 0;
+            for(int i=0; i <= n/2; i++)
+            {
+                sum += (n - 2*i) * i + 1;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanToInteger
+{
+    class Program
+    {
+        public static int RomanToInt(string s) {
+            var dict = new Dictionary<char, int>();
+            dict.Add('I', 1);
+            dict.Add('V', 5);
+            dict.Add('X', 10);
+            dict.Add('L', 50);
+            dict.Add('C', 100);
+            dict.Add('D', 500);
+            dict.Add('M', 1000);
+            int output = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(i != s.Length - 1)
+                    if(dict[s[i]] < dict[s[i+1]])
+                    {
+                        output += dict[s[i+1]] - dict[s[i]];
+                        i++; 
+                    }
+                    else
+                        output += dict[s[i]];
+                else
+                    output += dict[s[i]];
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("MCMXCIV"));
+        }
+    }
+}
