d6ab02a48d874bc2afbd78fa0af9e4cf974d73b3
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-10 08:05:06 +0300

    Solved a medium problem!!!!!

---
+++ NumberOfIslands/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfIslands
+{
+    class Program
+    {
+        public int NumIslands(char[][] grid) {
+            int numberOfIslands = 0;
+            for(int i = 0; i < grid.Length; i++){
+                for(int j = 0; j < grid[i].Length; j++){
+                    if(grid[i][j] == '0')
+                        continue;
+                    numberOfIslands++;
+                    checkNeibours(i, j);
+                }
+
+            }
+            return numberOfIslands;
+
+            void checkNeibours(int i, int j){
+                if(i + 1 < grid.Length){
+                    if(grid[i + 1][j] == '1'){
+                        grid[i+1][j] = '0';
+                        checkNeibours(i+1, j);
+                    }                    
+                }
+                if(i - 1 >= 0){
+                    if(grid[i - 1][j] == '1'){
+                        grid[i-1][j] = '0';
+                        checkNeibours(i-1, j);
+                    }
+                }
+                if(j + 1 < grid[i].Length){
+                    if(grid[i][j + 1] == '1'){
+                        grid[i][j+1] = '0';
+                        checkNeibours(i, j+1);
+                    }
+                }
+                if(j - 1 >= 0){
+                    if(grid[i][j - 1] == '1'){
+                        grid[i][j-1] = '0';
+                        checkNeibours(i, j-1);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -4,6 +4,13 @@ namespace ReverseString
 {
     class Program
     {
+        public static void ReverseString(char[] s) {
+            for(int i = 0; i < (s.Length - 1)/2; i++){
+                var aux = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = aux;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
