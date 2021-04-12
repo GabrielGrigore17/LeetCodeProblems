6c086b1d1951841d3324ec0bc5937145c4c1a972
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-12 08:36:06 +0300

    Completed a problem

---
+++ RunningSumOf1dArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RunningSumOf1dArray
+{
+    class Program
+    {
+        public int[] RunningSum(int[] nums) {
+            int sum = 0;
+            for(int i = 0; i < nums.Length; i++){
+                sum += nums[i];
+            }
+            return nums;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
