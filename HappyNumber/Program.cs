d6361a0f1e84192c37c93cde6af91545bba6a853
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-04-12 09:48:00 +0300

    Completed a problem

---
+++ HappyNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HappyNumber
+{
+    class Program
+    {
+        public static bool IsHappy(int n) {
+            var isLoop = new HashSet<int>();
+            while(n != 1){
+                int nCopy = n;
+                int nNew = 0;
+                while(nCopy != 0){
+                    nNew += (nCopy % 10) * (nCopy % 10);
+                    nCopy /= 10; 
+                }
+                Console.WriteLine(nNew);
+                if(!isLoop.Add(nNew))
+                    return false;
+                n = nNew;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsHappy(19));
+        }
+    }
+}
